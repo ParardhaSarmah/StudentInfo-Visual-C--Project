4e72681d999cfb5696e5131a2c698cec24754e3d
Parardha Sarmah <sarmahparardha@gmail.com>
2021-02-17 17:12:12 +0530

    First commit

---
+++ StudentInfo/UsefullStrings.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentInfo
+{
+    public class UsefullStrings
+    {
+        public static String GetString()
+        {
+            return "server=<SQLLOCALSERVERNAME>; initial catalog=soe; user id=<USERNAME>; password=<PASSWORD>";
+        }
+
+        public static String ErrorString()
+        {
+            return "An error has occurred at the database...\nMake sure that you haven't entered any invalid data..." +
+                "\n\nDetails below :\n";
+        }
+    }
+}
